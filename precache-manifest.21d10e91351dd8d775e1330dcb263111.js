f91596fe29e99eaf966ce6cff01dad9dd3330777
dinasvtr58 <dina60108@gmail.com>
2022-06-24 19:49:38 +0700

    Updates

---
+++ precache-manifest.21d10e91351dd8d775e1330dcb263111.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cefafc4e8d85b4a57175",
+    "url": "/TodoListReact/static/css/main.bec57256.chunk.css"
+  },
+  {
+    "revision": "cefafc4e8d85b4a57175",
+    "url": "/TodoListReact/static/js/main.44c83dac.chunk.js"
+  },
+  {
+    "revision": "5b33908ad625df18ce82",
+    "url": "/TodoListReact/static/js/runtime~main.b5a25205.js"
+  },
+  {
+    "revision": "eee8b6e6289e7f008d7d",
+    "url": "/TodoListReact/static/js/2.dad52260.chunk.js"
+  },
+  {
+    "revision": "6be1103fcca5d13705b2247d2a8f3c36",
+    "url": "/TodoListReact/index.html"
+  }
+];
